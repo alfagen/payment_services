8944cd0b71e5b207e70342077097a9e1f71f014b
Roman <roman0tershak@gmail.com>
2021-01-05 15:38:38 +0200

    Add constants

--- lib/payment_services/crypto_apis/payout_client.rb
+++ lib/payment_services/crypto_apis/payout_client.rb
@@ -4,6 +4,17 @@ require_relative 'client'
 
 class PaymentServices::CryptoApis
   class PayoutClient < PaymentServices::CryptoApis::Client
+    include AutoLogger
+    TIMEOUT = 10
+    API_URL = 'https://api.cryptoapis.io/v1'
+    NETWORK = 'testnet'
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    attr_reader :api_key
+
     def make_payout(inputs:, outputs:, fee:, wifs:)
       safely_parse http_request(
         url: "#{API_URL}/bc/bch/#{NETWORK}/txs/new",
