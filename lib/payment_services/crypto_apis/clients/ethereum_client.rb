6b566941fedaef0b2e53e3044739249f12e73aa6
Roman <roman0tershak@gmail.com>
2021-03-12 14:32:44 +0200

    Use mainnet

--- lib/payment_services/crypto_apis/clients/ethereum_client.rb
+++ lib/payment_services/crypto_apis/clients/ethereum_client.rb
@@ -11,18 +11,6 @@ class PaymentServices::CryptoApis
           method: :GET
         )
       end
-
-      private
-
-      def network
-        return 'mainnet' if Rails.env.production?
-
-        currency == 'eth' ? 'rinkeby' : 'mordor'
-      end
-
-      def base_url
-        "#{API_URL}/bc/#{currency}/#{network}"
-      end
     end
   end
 end
