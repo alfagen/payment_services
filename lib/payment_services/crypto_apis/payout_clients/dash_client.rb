94250d5cb5d3029e10ca321eeef03d5fa2be37cf
Roman <roman0tershak@gmail.com>
2021-03-13 16:53:27 +0200

    Implement BaseClient

--- lib/payment_services/crypto_apis/payout_clients/dash_client.rb
+++ lib/payment_services/crypto_apis/payout_clients/dash_client.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
-require_relative '../clients/dash_client'
+require_relative 'base_client'
 
 class PaymentServices::CryptoApis
   module PayoutClients
-    class DashClient < PaymentServices::CryptoApis::Clients::DashClient
+    class DashClient < PaymentServices::CryptoApis::PayoutClients::BaseClient
+      private
+
+      def base_url
+        "#{API_URL}/bc/dash/#{NETWORK}"
+      end
     end
   end
 end
