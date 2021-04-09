3ccb015dc676b8a26bf0cd433c752d8987f2dd2f
Roman <roman0tershak@gmail.com>
2021-04-09 20:39:52 +0300

    Add Appex payway

---
+++ lib/payment_services/appex_money/client.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require 'digest'
+
+class PaymentServices::AppexMoney
+  class Client
+    include AutoLogger
+    TIMEOUT = 10
+    API_URL = 'https://ecommerce.test.appexmoney.com/api/'
+
+    def initialize(account_id:, first_secret_key:, second_secret_key:)
+      @account_id = account_id
+      @first_secret_key = first_secret_key
+      @second_secret_key = second_secret_key
+    end
+
+    def create(params:)
+      params = params.merge(
+        account: account_id,
+        nonce: OpenSSL::Random.random_bytes(32)
+      )
+      params[:signature] = signature(params)
+
+      safely_parse http_request(
+        url: API_URL + 'payout/execute',
+        method: :POST,
+        body: params
+      )
+    end
+
+    def get(params:)
+      params = params.merge(
+        account: account_id,
+        nonce: OpenSSL::Random.random_bytes(32)
+      )
+      params[:signature] = signature(params)
+
+      safely_parse http_request(
+        url: API_URL + 'payout/status',
+        method: :POST,
+        body: params
+      )
+    end
+
+    private
+
+    attr_reader :account_id, :first_secret_key, :second_secret_key
+
+    def http_request(url:, method:, body: nil)
+      uri = URI.parse(url)
+      https = http(uri)
+      request = build_request(uri: uri, method: method, body: body)
+      logger.info "Request type: #{method} to #{uri} with payload #{request.body}"
+      https.request(request)
+    end
+
+    def build_request(uri:, method:, body: nil)
+      request = if method == :POST
+                  Net::HTTP::Post.new(uri.request_uri)
+                elsif method == :GET
+                  Net::HTTP::Get.new(uri.request_uri)
+                else
+                  raise "Запрос #{method} не поддерживается!"
+                end
+      request.body = (body.present? ? body : {}).to_json
+      request
+    end
+
+    def http(uri)
+      Net::HTTP.start(uri.host, uri.port,
+                      use_ssl: true,
+                      verify_mode: OpenSSL::SSL::VERIFY_NONE,
+                      open_timeout: TIMEOUT,
+                      read_timeout: TIMEOUT)
+    end
+
+    def signature(params)
+      sign_string = ''
+
+      params.each do |_k, v| 
+        sign_string += "#{v}:"
+      end
+
+      sign_string = sign_string.upcase + "#{first_secret_key}:#{second_secret_key}"
+
+      Digest::MD5.hexdigest sign_string
+    end
+
+    def safely_parse(response)
+      res = JSON.parse(response.body).with_indifferent_access
+      logger.info "Response: #{res}"
+      res
+    rescue JSON::ParserError => err
+      logger.warn "Request failed #{response.class} #{response.body}"
+      Bugsnag.notify err do |report|
+        report.add_tab(:response, response_class: response.class, response_body: response.body)
+      end
+      response.body
+    end
+  end
+end
---
+++ lib/payment_services/appex_money/payout_adapter.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require_relative 'payout'
+require_relative 'client'
+
+class PaymentServices::AppexMoney
+  class PayoutAdapter < ::PaymentServices::Base::PayoutAdapter
+    def make_payout!(amount:, payment_card_details:, transaction_id:, destination_account:, order_payout_id:)
+      make_payout(
+        amount: amount,
+        destination_account: destination_account,
+        order_payout_id: order_payout_id
+      )
+    end
+
+    def refresh_status!(payout_id)
+      @payout_id = payout_id
+      return if payout.pending?
+
+      params = {
+        number: payout.number
+      }
+
+      response = client.get(params: params)
+      raise "Can't get order details: #{response[:errortext]}" if response.dig(:errortext)
+
+      payout.update!(status: response[:status]) if response[:status]
+      payout.confirm! if payout.success?
+      payout.fail! if payout.status_failed?
+
+      result
+    end
+
+    def payout
+      @payout ||= Payout.find_by(id: payout_id)
+    end
+
+    private
+
+    attr_accessor :payout_id
+
+    def make_payout(amount:, destination_account:, order_payout_id:)
+      @payout_id = Payout.create!(amount: amount, destination_account: destination_account, order_payout_id: order_payout_id).id
+
+      params = {
+        amount: amount.to_s,
+        amountcurr: wallet.currency.to_s.upcase,
+        number: "Invoice#{@payout_id}",
+        operator: wallet.payment_system.payway,
+        params: destination_account
+      }
+      response = client.create(params: params)
+      raise "Can't process payout: #{response[:errortext]}" if response.dig(:errortext)
+
+      payout.pay!(number: response[:number]) if response[:number]
+    end
+
+    def client
+      @client ||= begin
+        Client.new(
+          account_id: wallet.merchant_id,
+          first_secret_key: wallet.api_key,
+          second_secret_key: wallet.secretKey
+        )
+      end
+    end
+  end
+end
