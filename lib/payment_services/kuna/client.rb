cf7eb0d26093cf9dcdb23d3bce6436f3234bd723
Roman <roman0tershak@gmail.com>
2021-04-21 20:59:15 +0300

    Fix

--- lib/payment_services/kuna/client.rb
+++ lib/payment_services/kuna/client.rb
@@ -83,7 +83,7 @@ class PaymentServices::Kuna
     end
 
     def safely_parse(response)
-      res = JSON.parse(response.body).with_indifferent_access
+      res = JSON.parse(response.body)
       logger.info "Response: #{res}"
       res
     rescue JSON::ParserError => err
