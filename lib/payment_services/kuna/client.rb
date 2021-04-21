47f67e376f3ead107dafe85df047d5a88affd716
Roman <roman0tershak@gmail.com>
2021-04-21 18:43:40 +0300

    Add Kuna payway

---
+++ lib/payment_services/kuna/client.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require 'digest'
+
+class PaymentServices::Kuna
+  class Client
+    include AutoLogger
+    TIMEOUT = 10
+    API_URL = 'https://api.kuna.io'
+    GATEWAY = 'default'
+
+    def initialize(api_key:, secret_key:)
+      @api_key = api_key
+      @secret_key = secret_key
+    end
+
+    def create(params:)
+      params = params.merge(
+        gateway: GATEWAY
+      )
+
+      safely_parse http_request(
+        url: API_URL + '/v3/auth/withdraw',
+        method: :POST,
+        body: params
+      )
+    end
+
+    def get(params:)
+      safely_parse http_request(
+        url: API_URL + '/v3/withdraw/details',
+        method: :POST,
+        body: params
+      )
+    end
+
+    private
+
+    attr_reader :api_key, :secret_key
+
+    def http_request(url:, method:, body: nil)
+      uri = URI.parse(url)
+      https = http(uri)
+      request = build_request(uri: uri, method: method, body: body)
+      logger.info "Request type: #{method} to #{uri} with payload #{request.body}"
+      https.request(request)
+    end
+
+    def build_request(uri:, method:, body: nil)
+      request = if method == :POST
+                  Net::HTTP::Post.new(uri.request_uri, headers(uri.to_s, body))
+                elsif method == :GET
+                  Net::HTTP::Get.new(uri.request_uri)
+                else
+                  raise "Запрос #{method} не поддерживается!"
+                end
+      request.body = (body.present? ? body : {}).to_json
+      request
+    end
+
+    def headers(url, params)
+      nonce = Time.now.to_i.to_s
+
+      {
+        'Content-Type': 'application/json',
+        'kun-nonce'     : nonce,
+        'kun-apikey'    : api_key,
+        'kun-signature' : signature(url, params, nonce)
+      }
+    end
+
+    def http(uri)
+      Net::HTTP.start(uri.host, uri.port,
+                      use_ssl: true,
+                      verify_mode: OpenSSL::SSL::VERIFY_NONE,
+                      open_timeout: TIMEOUT,
+                      read_timeout: TIMEOUT)
+    end
+
+    def signature(url, params, nonce)
+      url.slice!(API_URL)
+      sign_string = url + nonce + params.to_json
+
+      OpenSSL::HMAC.hexdigest('SHA384', secret_key, sign_string)
+    end
+
+    def safely_parse(response)
+      res = JSON.parse(response.body).with_indifferent_access
+      logger.info "Response: #{res}"
+      res
+    rescue JSON::ParserError => err
+      logger.warn "Request failed #{response.class} #{response.body}"
+      Bugsnag.notify err do |report|
+        report.add_tab(:response, response_class: response.class, response_body: response.body)
+      end
+      response.body
+    end
+  end
+end
---
+++ lib/payment_services/kuna/payout_adapter.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require_relative 'payout'
+require_relative 'client'
+
+class PaymentServices::Kuna
+  class PayoutAdapter < ::PaymentServices::Base::PayoutAdapter
+    def make_payout!(amount:, payment_card_details:, transaction_id:, destination_account:, order_payout_id:)
+      make_payout(
+        amount: amount,
+        destination_account: destination_account,
+        order_payout_id: order_payout_id
+      )
+    end
+
+    def refresh_status!(payout_id)
+      @payout_id = payout_id
+      return if payout.pending?
+
+      response = client.get(params: { withdrawal_id: payout.withdrawal_id })
+      raise "Can't get withdrawal details: #{response[:messages].join(', ')}" if response.dig(:messages)
+
+      payout.update!(status: response[:status]) if response[:status]
+      payout.confirm! if payout.success?
+      payout.fail! if payout.status_failed?
+
+      response
+    end
+
+    def payout
+      @payout ||= Payout.find_by(id: payout_id)
+    end
+
+    private
+
+    attr_accessor :payout_id
+
+    def make_payout(amount:, destination_account:, order_payout_id:)
+      @payout_id = Payout.create!(amount: amount, destination_account: destination_account, order_payout_id: order_payout_id).id
+
+      params = {
+        amount: amount.to_d,
+        downcase: wallet.currency.to_s.downcase,
+        withdraw_to: destination_account
+      }
+      response = client.create(params: params)
+      raise "Can't process payout: #{response[:messages].join(', ')}" if response.dig(:messages)
+
+      payout.pay!(withdrawal_id: response[:withdrawal_id]) if response[:withdrawal_id]
+    end
+
+    def client
+      @client ||= begin
+        Client.new(
+          api_key: wallet.api_key,
+          secret_key: wallet.api_secret
+        )
+      end
+    end
+  end
+end
