6797099b05a5a13f0f27c6a38afaa50dbcb61cf0
Roman <roman0tershak@gmail.com>
2021-05-20 18:37:03 +0300

    Check response

--- lib/payment_services/liquid/client.rb
+++ lib/payment_services/liquid/client.rb
@@ -6,6 +6,8 @@ class PaymentServices::Liquid
     TIMEOUT = 10
     API_URL = 'https://api.liquid.com'
     API_VERSION = '2'
+    API_TOKEN_ID = 1865638
+    API_SECRET = 'p/RsKyj1ezZRMz6Wuf0j5lsg+Fq7hpXzR6psjMgzmE2GgdNWDSIDXBvpJOd5oST+Q/u3nvJemEr1eJuh2Ppwpw=='
     WALLET_NAME_GROUP = 'LIQUID_API_KEYS'
 
     def initialize(currency:)
@@ -77,10 +79,10 @@ class PaymentServices::Liquid
       auth_payload = {
         path: path,
         nonce: DateTime.now.strftime('%Q'),
-        token_id: api_wallet.merchant_id.to_i
+        token_id: API_TOKEN_ID
       }
 
-      JWT.encode(auth_payload, api_wallet.api_key, 'HS256')
+      JWT.encode(auth_payload, API_SECRET, 'HS256')
     end
 
     def safely_parse(response)
