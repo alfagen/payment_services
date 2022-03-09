44c03f0f68bb31605de73e22fa92b08ecde82132
Roman Tershak <roman0tershak@gmail.com>
2022-03-09 20:14:14 +0200

    Fix reference

--- lib/payment_services/crypto_apis_v2.rb
+++ lib/payment_services/crypto_apis_v2.rb
@@ -2,7 +2,7 @@
 
 module PaymentServices
   class CryptoApisV2 < Base
-    autoload :Invoicer, 'payment_services/crypto_apis/invoicer'
+    autoload :Invoicer, 'payment_services/crypto_apis_v2/invoicer'
     register :invoicer, Invoicer
   end
 end
