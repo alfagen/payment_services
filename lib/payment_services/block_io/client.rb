94eab922b1c3a458ad9bd0baffcd4fe09c27ce6a
Roman Tershak <roman0tershak@gmail.com>
2022-04-14 16:08:56 +0300

    Add log info

--- lib/payment_services/block_io/client.rb
+++ lib/payment_services/block_io/client.rb
@@ -8,6 +8,7 @@ class PaymentServices::BlockIo
   class Client
     include AutoLogger
     Error = Class.new StandardError
+    API_VERSION = 2
 
     def initialize(api_key:, pin:)
       @api_key = api_key
@@ -15,19 +16,27 @@ class PaymentServices::BlockIo
     end
 
     def make_payout(address:, amount:, nonce:)
+      logger.info "---- Request payout to: #{address}, on #{amount} ----"
       begin
         prepare_response = client.prepare_transaction(amounts: amount, to_addresses: address)
         sign_response = client.create_and_sign_transaction(prepare_response)
         submit_response = client.submit_transaction(transaction_data: sign_response)
+        logger.info "---- Response: #{submit_response.to_s} ----"
+        submit_response
       rescue Exception => error # BlockIo uses Exceptions instead StandardError
+        logger.error error.to_s
         raise Error, error.to_s
       end
     end
 
     def transactions(address)
+      logger.info "---- Request transactions info on #{address} ----"
       begin
-        client.get_transactions(type: 'sent', addresses: address)
+        transactions = client.get_transactions(type: 'sent', addresses: address)
+        logger.info "---- Response: #{transactions} ----"
+        transactions
       rescue Exception => error
+        logger.error error.to_s
         raise Error, error.to_s
       end
     end
@@ -35,7 +44,7 @@ class PaymentServices::BlockIo
     private
 
     def client
-      @client ||= BlockIo::Client.new(api_key: api_key, pin: pin, version: 2)
+      @client ||= BlockIo::Client.new(api_key: api_key, pin: pin, version: API_VERSION)
     end
 
     attr_reader :api_key, :pin
