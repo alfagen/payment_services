63a77cf828bb5c3a3a00943ce8bd5a9da4ca450b
Roman Tershak <roman0tershak@gmail.com>
2022-04-22 17:40:26 +0300

    Fix Transaction builder

--- lib/payment_services/block_io/transaction.rb
+++ lib/payment_services/block_io/transaction.rb
@@ -8,36 +8,30 @@ class PaymentServices::BlockIo
 
     attribute :id, String
     attribute :confirmations, Integer
-    attribute :created_at, DateTime
-    attribute :total_spend, Float
     attribute :source, String
 
     def self.build_from(raw_transaction:)
       new(
         id: raw_transaction['txid'],
         confirmations: raw_transaction['confirmations'],
-        created_at: timestamp_in_datetime_utc(raw_transaction['time']),
-        total_spend: parse_total_spend(raw_transaction),
         source: raw_transaction
       )
     end
 
     def to_s
-      attributes.to_s
+      source.to_s
     end
 
     def successful?
       confirmations >= CONFIRMATIONS_FOR_COMPLETE
     end
 
-    private
-
-    def timestamp_in_datetime_utc(timestamp)
-      Time.at(timestamp).to_datetime.utc
+    def created_at
+      Time.at(source['time']).to_datetime.utc
     end
 
-    def parse_total_spend(raw_transaction)
-      raw_transaction['total_amount_sent']&.to_f || 0
+    def total_spend
+      source['total_amount_sent'].to_f
     end
   end
 end
