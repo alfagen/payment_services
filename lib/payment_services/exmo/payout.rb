04473dbd8a820223a5442c88f3565fd7cb9e080a
Roman Tershak <roman0tershak@gmail.com>
2022-05-10 18:17:30 +0300

    Exmo payouts: track transaction id

--- lib/payment_services/exmo/payout.rb
+++ lib/payment_services/exmo/payout.rb
@@ -11,6 +11,8 @@ class PaymentServices::Exmo
     monetize :amount_cents, as: :amount
     validates :amount_cents, :destination_account, :state, presence: true
 
+    alias_attribute :txid, :transaction_id
+
     workflow_column :state
     workflow do
       state :pending do
@@ -28,15 +30,18 @@ class PaymentServices::Exmo
       update(task_id: task_id)
     end
 
-    def update_state_by_provider(state)
-      update!(provider_state: state)
-
-      confirm!  if success?
-      fail!     if status_failed?
-    end
-
     def order_fio
       order_payout.order.outcome_fio.presence || order_payout.order.outcome_unk
+    end
+
+    def update_payout_details!(transaction:)
+      update!(
+        provider_state: transaction.provider_state,
+        transaction_id: transaction.id
+      )
+
+      confirm!  if transaction.successful?
+      fail!     if transaction.failed?
     end
 
     private
@@ -44,13 +49,16 @@ class PaymentServices::Exmo
     def order_payout
       @order_payout ||= OrderPayout.find(order_payout_id)
     end
+  end
+end
 
-    def success?
-      provider_state == 'Paid'
-    end
-
-    def status_failed?
-      provider_state == 'Cancelled' || provider_state == 'Error'
-    end
+def scan_number(number)
+  count_of_digits = number.to_s.length
+  if number.positive?
+    "Positive: #{count_of_digits} digits"
+  elsif number.negative?
+    "Negative: #{count_of_digits - 1} digits"
+  else 
+    'Zero'
   end
 end
