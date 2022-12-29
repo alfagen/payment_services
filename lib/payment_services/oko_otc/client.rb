934c5d003d156ebfc321df8ea6aa1065b337853d
Roman Tershak <roman0tershak@gmail.com>
2022-12-29 14:00:38 +0200

    Payout Adapter for Oko Otc

---
+++ lib/payment_services/oko_otc/client.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class PaymentServices::OkoOtc
+  class Client < ::PaymentServices::Base::Client
+    API_URL = 'https://oko-otc.ru/api/v2/payment'
+
+    def initialize(api_key:, secret_key:)
+      @api_key    = api_key
+      @secret_key = secret_key
+    end
+
+    def process_payout(params:)
+      safely_parse http_request(
+        url: "#{API_URL}/create_withdraw",
+        method: :POST,
+        body: params.to_json,
+        headers: build_headers(build_signature(params))
+      )
+    end
+
+    def payout_status(params:)
+      safely_parse http_request(
+        url: "#{API_URL}/fetch_orders_v2",
+        method: :POST,
+        body: params.to_json,
+        headers: build_headers('')
+      )
+    end
+
+    private
+
+    attr_reader :api_key, :secret_key
+
+    def build_signature(request_body)
+      sign_string = "#{request_body[:sum]};#{request_body[:wallet]};#{request_body[:orderUID]}"
+      OpenSSL::HMAC.hexdigest('SHA512', secret_key, sign_string)
+    end
+
+    def build_headers(signature)
+      {
+        'Content-Type'  => 'application/json',
+        'Accept'        => 'application/json',
+        'Authorization' => api_key,
+        'Signature'     => signature
+      }
+    end
+  end
+end
---
+++ lib/payment_services/oko_otc/payout_adapter.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require_relative 'payout'
+require_relative 'client'
+
+class PaymentServices::OkoOtc
+  class PayoutAdapter < ::PaymentServices::Base::PayoutAdapter
+    Error = Class.new StandardError
+    PayoutCreateRequestFailed = Class.new Error
+    WithdrawHistoryRequestFailed = Class.new Error
+
+    def make_payout!(amount:, payment_card_details:, transaction_id:, destination_account:, order_payout_id:)
+      make_payout(
+        amount: amount,
+        destination_account: destination_account,
+        order_payout_id: order_payout_id
+      )
+    end
+
+    def refresh_status!(payout_id)
+      payout = Payout.find(payout_id)
+      return if payout.pending?
+
+      response = client.payout_status(params: { orderID: payout.withdrawal_id, orderType: 'withdraw' })
+
+      raise WithdrawHistoryRequestFailed, "Can't get withdraw history: #{response['errCode']}" unless response['status']
+
+      payout.update_state_by_provider(response['statusName'])
+      response
+    end
+
+    private
+
+    def make_payout(amount:, destination_account:, order_payout_id:)
+      payout = Payout.create!(amount: amount, destination_account: destination_account, order_payout_id: order_payout_id)
+      public_order_id = OrderPayout.find(order_payout_id).order.public_id
+
+      params = {
+        sum: amount.to_i,
+        currencyFrom: amount.currency.to_s,
+        wallet: destination_account,
+        bank: amount.currency.to_s,
+        cardExpiration: nil,
+        orderUID: "#{public_order_id}-#{order_payout_id}"
+      }
+      response = client.process_payout(params: params)
+      raise PayoutCreateRequestFailed, "Can't create payout: #{response['errCode']}" unless response['status']
+
+      payout.pay!(withdrawal_id: response['orderID'])
+    end
+
+    def client
+      @client ||= begin
+        Client.new(api_key: wallet.outcome_api_key, secret_key: wallet.outcome_api_secret)
+      end
+    end
+  end
+end
