2e338d3ccaf5c00950ec03729a8e3ae2537d479f
Roman Tershak <roman0tershak@gmail.com>
2023-02-07 15:10:32 +0200

    F

--- lib/payment_services/paylama_crypto/invoice.rb
+++ lib/payment_services/paylama_crypto/invoice.rb
@@ -24,7 +24,7 @@ class PaymentServices::PaylamaCrypto
     end
 
     def update_state_by_transaction(transaction)
-      raise "#{amount.to_f} is needed. But #{transaction.amount} has come." if transaction.valid_amount?(amount)
+      raise "#{amount.to_f} is needed. But #{transaction.amount} has come." unless transaction.valid_amount?(amount)
       has_transaction! if pending?
       update!(
         provider_state: transaction.status, 
