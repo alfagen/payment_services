0591562d58c2b5a7515cbadada6f864480860e25
Roman Tershak <roman0tershak@gmail.com>
2023-04-23 00:54:27 +0300

    Fix <

--- lib/payment_services/ex_pay/payout.rb
+++ lib/payment_services/ex_pay/payout.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PaymentServices::ExPay
-  class Payout < ApplicationRecord
+  class Payout < ::PaymentServices::Base::FiatPayout
     self.table_name = 'ex_pay_payouts'
 
     monetize :amount_cents, as: :amount
