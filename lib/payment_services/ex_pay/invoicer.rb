072ecc7f5ade9b1f581850f39b5ee3d922654b54
Roman Tershak <roman0tershak@gmail.com>
2023-05-16 15:02:10 +0300

    Fix wallet addresses

--- lib/payment_services/ex_pay/invoicer.rb
+++ lib/payment_services/ex_pay/invoicer.rb
@@ -7,16 +7,19 @@ class PaymentServices::ExPay
   class Invoicer < ::PaymentServices::Base::Invoicer
     INVOICE_PROVIDER_TOKEN = 'CARDRUBP2P'
 
-    def create_invoice(money)
-      Invoice.create!(amount: money, order_public_id: order.public_id)
+    def wallet_information
       response = client.create_invoice(params: invoice_params)
-
       raise "Can't create invoice: #{response['description']}" unless response['status'] == Invoice::INITIAL_PROVIDER_STATE
 
       invoice.update!(
         deposit_id: response['tracker_id'],
         pay_url: response['alter_refer']
       )
+      response['refer']
+    end
+
+    def create_invoice(money)
+      Invoice.create!(amount: money, order_public_id: order.public_id, address: order.income_account_emoney)
     end
 
     def pay_invoice_url
