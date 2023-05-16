079ea417d06646c96ea1d98b6c728b4beb9a8ad3
Roman Tershak <roman0tershak@gmail.com>
2023-05-16 17:16:09 +0300

    Check tinkoff

--- lib/payment_services/paylama/invoicer.rb
+++ lib/payment_services/paylama/invoicer.rb
@@ -6,7 +6,7 @@ require_relative 'currency_repository'
 
 class PaymentServices::Paylama
   class Invoicer < ::PaymentServices::Base::Invoicer
-    P2P_BANK_NAME = 'sberbank'
+    P2P_BANK_NAME = 'tinkoff'
 
     def wallet_information(po:)
       response = client.generate_p2p_invoice(params: invoice_p2p_params(po))
