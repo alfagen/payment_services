bf0f57c89935ea5f8a0ea387271475eb875b87a6
Roman Tershak <roman0tershak@gmail.com>
2023-07-07 22:06:04 +0300

    Fix sign

--- lib/payment_services/any_pay/client.rb
+++ lib/payment_services/any_pay/client.rb
@@ -44,7 +44,7 @@ class PaymentServices::AnyPay
     end
 
     def build_signature(api_method_name, params)
-      sign_string = api_method_name + secret_key + [params[:project_id], params[:pay_id], params[:amount], params[:currency], params[:desc], params[:method]].join + api_key
+      sign_string = api_method_name + ":#{secret_key}:" + [params[:project_id], params[:pay_id], params[:amount], params[:currency], params[:desc], params[:method]].join(':') + ":#{api_key}"
       Digest::SHA256.hexdigest(sign_string)
     end
   end
