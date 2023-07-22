df0e2f132f0a2eaebc755aca73d74e4b364716c6
Roman Tershak <roman0tershak@gmail.com>
2023-07-22 14:56:48 +0300

    Fix signature

--- lib/payment_services/coin_payments_hub/client.rb
+++ lib/payment_services/coin_payments_hub/client.rb
@@ -32,28 +32,22 @@ class PaymentServices::CoinPaymentsHub
     end
 
     def sign_params(params)
-      md5_api_key = Digest::MD5.hexdigest(api_key)
+      md5_api_key = Digest::MD5.hexdigest('1qaz2wsx')
       cipher = OpenSSL::Cipher.new("aes-256-cbc")
       cipher.encrypt
       iv = cipher.iv = cipher.random_iv
-      salt = OpenSSL::Random.random_bytes(32)
-      iter = 10000
-      key_len = cipher.key_len
-      digest = OpenSSL::Digest::SHA256.new
-      key = OpenSSL::PKCS5.pbkdf2_hmac(md5_api_key, salt, iter, key_len, digest)
-      cipher.key = key
+      cipher.key = md5_api_key
 
-      iv = Base64.urlsafe_encode64(iv)
-      # iv = Base64.encode64(iv)
       value = cipher.update(params.to_json) + cipher.final
-      value = value.force_encoding("ISO-8859-1").encode("UTF-8")
-      mac = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), iv, md5_api_key)
+      value = Base64.encode64(value).chomp
+      iv = Base64.encode64(iv).chomp
+      mac = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), md5_api_key, iv + value)
       tag = ''
 
       json_string = { iv: iv, value: value, mac: mac, tag: tag }.to_json
-      json_string = json_string.force_encoding("UTF-8").encode("ISO-8859-1")
-      # Base64.encode64(json_string)
-      Base64.urlsafe_encode64(json_string)
+      Base64.encode64(json_string).gsub(/\n/, '')
     end
+
+    sign_params
   end
 end
