fa9761165652c5c7ce15be8e8afad24cdfc445da
Roman Tershak <roman0tershak@gmail.com>
2023-10-11 09:39:04 +0300

    Fix

--- lib/payment_services/ex_pay/invoicer.rb
+++ lib/payment_services/ex_pay/invoicer.rb
@@ -3,6 +3,8 @@
 require_relative 'invoice'
 require_relative 'client'
 
+56973f6f894ded8547d2b9d1ef9991ba3ab4232734e3a584b12168c6295b545f19ae9f7832627c846ecd4dbbaab04653454b750f388c128c8527837702997400
+
 class PaymentServices::ExPay
   class Invoicer < ::PaymentServices::Base::Invoicer
     PROVIDER_TOKEN = 'CARDRUBP2P'
@@ -10,7 +12,10 @@ class PaymentServices::ExPay
     MERCHANT_ID = '1'
 
     def prepare_invoice_and_get_wallet!(currency:, token_network:)
+      create_invoice!
       response = client.create_invoice(params: invoice_p2p_params)
+
+      invoice.update!(deposit_id: response['tracker_id'])
       PaymentServices::Base::Wallet.new(address: response['refer'], name: response.dig('extra_info', 'recipient_name'))
     end
 
@@ -35,6 +40,10 @@ class PaymentServices::ExPay
 
     delegate :income_payment_system, to: :order
     delegate :callback_url, to: :income_payment_system
+
+    def create_invoice!
+      Invoice.create!(amount: order.calculated_income_money, order_public_id: order.public_id)
+    end
 
     def invoice_p2p_params
       {
