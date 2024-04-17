3a4c82d3a0cf814248e988b8ac41242ffeef9edd
Roman Tershak <roman0tershak@gmail.com>
2024-04-17 16:58:44 +0300

    fix

--- lib/payment_services/your_payments.rb
+++ lib/payment_services/your_payments.rb
@@ -3,12 +3,8 @@
 module PaymentServices
   class YourPayments < Base
     autoload :Invoicer, 'payment_services/your_payments/invoicer'
-<<<<<<< HEAD
     autoload :PayoutAdapter, 'payment_services/your_payments/payout_adapter'
     register :invoicer, Invoicer
     register :payout_adapter, PayoutAdapter
-=======
-    register :invoicer, Invoicer
->>>>>>> master
   end
 end
