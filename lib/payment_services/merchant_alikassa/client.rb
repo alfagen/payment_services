44cbc2013831640a11f1ec026a1742aeb96ec118
Roman Tershak <roman0tershak@gmail.com>
2024-05-08 02:46:18 +0300

    Alikassa: выплаты

--- lib/payment_services/merchant_alikassa/client.rb
+++ lib/payment_services/merchant_alikassa/client.rb
@@ -3,7 +3,8 @@
 class PaymentServices::MerchantAlikassa
   class Client < ::PaymentServices::Base::Client
     API_URL = 'https://api-merchant.alikassa.com/v1'
-    PRIVATE_KEY_FILE_PATH = 'config/alikassa_payments_privatekey.pem'
+    PAYMENTS_PRIVATE_KEY_FILE_PATH = 'config/alikassa_payments_privatekey.pem'
+    PAYOUTS_PRIVATE_KEY_FILE_PATH = 'config/alikassa_payouts_privatekey.pem'
 
     def initialize(api_key:, secret_key:)
       @api_key = api_key
@@ -15,17 +16,36 @@ class PaymentServices::MerchantAlikassa
         url: "#{API_URL}/payment",
         method: :POST,
         body: params.to_json,
-        headers: build_headers(signature: build_signature(params))
+        headers: build_headers(signature: build_signature(params, PAYMENTS_PRIVATE_KEY_FILE_PATH))
       )
     end
 
-    def transaction(deposit_id:)
+    def invoice_transaction(deposit_id:)
       params = { id: deposit_id }
       safely_parse http_request(
         url: "#{API_URL}/payment/status",
         method: :POST,
         body: params.to_json,
-        headers: build_headers(signature: build_signature(params))
+        headers: build_headers(signature: build_signature(params, PAYMENTS_PRIVATE_KEY_FILE_PATH))
+      )
+    end
+
+    def create_payout(params:)
+      safely_parse http_request(
+        url: "#{API_URL}/payout",
+        method: :POST,
+        body: params.to_json,
+        headers: build_headers(signature: build_signature(params, PAYOUTS_PRIVATE_KEY_FILE_PATH))
+      )
+    end
+
+    def payout_transaction(payout_id:)
+      params = { id: payout_id }
+      safely_parse http_request(
+        url: "#{API_URL}/payout/status",
+        method: :POST,
+        body: params.to_json,
+        headers: build_headers(signature: build_signature(params, PAYOUTS_PRIVATE_KEY_FILE_PATH))
       )
     end
 
@@ -41,8 +61,8 @@ class PaymentServices::MerchantAlikassa
       }
     end
 
-    def build_signature(params)
-      private_key = OpenSSL::PKey::read(File.read(PRIVATE_KEY_FILE_PATH), secret_key)
+    def build_signature(params, private_key_file_path)
+      private_key = OpenSSL::PKey::read(File.read(private_key_file_path), secret_key)
       signature = private_key.sign(OpenSSL::Digest::SHA1.new, params.to_json)
       Base64.encode64(signature).gsub(/\n/, '')
     end
---
+++ lib/payment_services/merchant_alikassa/payout.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class PaymentServices::MerchantAlikassa
+  class Payout < ::PaymentServices::Base::FiatPayout
+    self.table_name = 'merchant_alikassa_payouts'
+
+    monetize :amount_cents, as: :amount
+
+    private
+
+    def provider_succeed?
+      provider_state == Invoice::SUCCESS_PROVIDER_STATE
+    end
+
+    def provider_failed?
+      provider_state.in? Invoice::FAILED_PROVIDER_STATES
+    end
+  end
+end
