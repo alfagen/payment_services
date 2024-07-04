83f47a30f685c858eed2a5d4e2ece026e571acc5
Roman Tershak <roman0tershak@gmail.com>
2024-07-05 02:55:00 +0300

    F

--- lib/payment_services/base/p2p_bank_resolver.rb
+++ lib/payment_services/base/p2p_bank_resolver.rb
@@ -397,7 +397,7 @@ class PaymentServices::Base::P2pBankResolver
   end
 
   def sbp_bank
-    PAYWAY_TO_SBP_BANK.dig(adapter_class_name, direction, sbp_client_field) || 
+    PAYWAY_TO_SBP_BANK.dig(adapter_class_name, direction, sbp_client_field).presence || 
     PAYWAY_TO_SBP_BANK.dig(adapter_class_name, direction, sbp_client_field_new)
   end
 
@@ -411,7 +411,7 @@ class PaymentServices::Base::P2pBankResolver
 
   delegate :income_sbp, to: :income_payment_system
   delegate :outcome_sbp, to: :outcome_payment_system
-  delegate :income_currency, :income_payment_system, :outcome_currency, :outcome_payment_system, :income_unk, :outcome_unk, :income_bank, :outcome_bank, to: :order
+  delegate :income_currency, :income_payment_system, :outcome_currency, :outcome_payment_system, :income_unk, :outcome_unk, :income_bank_name, :outcome_bank_name, to: :order
 
   def order
     @order ||= direction.inquiry.income? ? adapter.order : adapter.wallet_transfers.first.order_payout.order
@@ -430,7 +430,7 @@ class PaymentServices::Base::P2pBankResolver
   end
 
   def sbp_client_field_new
-    @sbp_client_field ||= send("#{direction}_bank")
+    @sbp_client_field ||= send("#{direction}_bank_name")
   end
 
   def sbp_checkbox
