2a6d8935aa472dffbd544cc25075b3c5ea8f2500
Roman Tershak <roman0tershak@gmail.com>
2024-07-29 17:16:30 +0300

    Erapay: прием платежей

---
+++ lib/payment_services/erapay/client.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class PaymentServices::Erapay
+  class Client < ::PaymentServices::Base::Client
+    API_URL = 'https://erapay.ru/api'
+
+    def initialize(api_key:, secret_key:)
+      @api_key = api_key
+      @secret_key = secret_key
+    end
+
+    def create_invoice(params:)
+      safely_parse http_request(
+        url: "#{API_URL}/createOrder",
+        method: :POST,
+        body: URI.encode_www_form(params.merge(token: api_key, shop_id: secret_key)),
+        headers: build_headers
+      )
+    end
+
+    private
+
+    attr_reader :api_key, :secret_key
+
+    def build_headers
+      {
+        'Content-Type' => 'application/x-www-form-urlencoded'
+      }
+    end
+  end
+end
