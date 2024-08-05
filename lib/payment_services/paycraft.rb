94a16eeaae58af40deb46f556621d846d25e6880
Roman <roman0tershak@gmail.com>
2024-08-05 21:02:09 +0300

    Paycraft: выплаты (#151)

--- lib/payment_services/paycraft.rb
+++ lib/payment_services/paycraft.rb
@@ -3,6 +3,9 @@
 module PaymentServices
   class Paycraft < Base
     autoload :Invoicer, 'payment_services/paycraft/invoicer'
+    autoload :PayoutAdapter, 'payment_services/paycraft/payout_adapter'
+
     register :invoicer, Invoicer
+    register :payout_adapter, PayoutAdapter
   end
 end
---
+++ lib/payment_services/paycraft/payout.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class PaymentServices::Paycraft
+  class Payout < ::PaymentServices::Base::FiatPayout
+    SUCCESS_PROVIDER_STATE = 2
+    FAILED_PROVIDER_STATE  = 3
+
+    self.table_name = 'paycraft_payouts'
+
+    monetize :amount_cents, as: :amount
+
+    private
+
+    def provider_succeed?
+      provider_state == SUCCESS_PROVIDER_STATE
+    end
+
+    def provider_failed?
+      provider_state == FAILED_PROVIDER_STATE
+    end
+  end
+end
