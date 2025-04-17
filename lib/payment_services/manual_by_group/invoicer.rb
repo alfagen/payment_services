e830f322df1b29e324970f977052947d7d1345a3
Roman <roman0tershak@gmail.com>
2025-04-17 14:08:47 +0300

    Доработки по ManualByGroup

--- lib/payment_services/manual_by_group/invoicer.rb
+++ lib/payment_services/manual_by_group/invoicer.rb
@@ -3,7 +3,7 @@
 class PaymentServices::ManualByGroup
   class Invoicer < ::PaymentServices::Base::Invoicer
     def prepare_invoice_and_get_wallet!(currency:, token_network:)
-      wallet = income_payment_system.select_next_wallet!(income_payment_system.wallets_available_for_transfers.where(name_group: income_payment_system.wallets_name_group.presence))
+      wallet = income_payment_system.select_next_wallet!(income_payment_system.wallets_available_for_transfers.income.where(name_group: income_payment_system.wallets_name_group.presence))
       PaymentServices::Base::Wallet.new(
         address: wallet&.account,
         name: wallet&.name,
